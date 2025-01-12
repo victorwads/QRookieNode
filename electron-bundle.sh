b68cb838d81f0f633da2159c427c6a904b3ee4cb
Victor Wads <victor.laureano@gmail.com>
2025-01-12 11:10:26 -0300

    build config

--- electron-bundle.sh
+++ electron-bundle.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-rm -rf dist
+if [ -x "$(command -v snapcraft)" ]; then
+    brew install snapcraft
+fi
+
+# rm -rf dist
+yarn install
 yarn react:build
 yarn electron:build
-electron-builder
+electron-builder --macos &
+MACOS=$!
+electron-builder --linux &
+LINUX=$!
+electron-builder --win
+WIN=$!
+
+wait $WIN $MACOS $LINUX
 
 # if not ci, open macos app
 if [ "$CI" != "true" ]; then

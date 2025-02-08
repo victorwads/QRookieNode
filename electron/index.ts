e063fc7fbf338788bc38aad9071b890da997b833
Victor Wads <victor.laureano@gmail.com>
2025-02-08 02:15:34 -0300

    fix electron progress

--- electron/index.ts
+++ electron/index.ts
@@ -2,20 +2,18 @@ import { GameStatusInfo } from "./comands/games";
 import log from "./log";
 import { sendInfo as sendInfoNode } from "./main/node";
 
-let electron: typeof import("./main/electron/bridge") | null = null;
 
+let sendInfoElectron: ((info: GameStatusInfo) => void) | null = null;
 if (process.versions.electron) {
   try {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
-    electron = require("./main/electron");
+    sendInfoElectron = require("./main/electron/bridge").sendInfo;
   } catch (err) {
     log.error("Failed to load Electron module:", err);
   }
 }
 
 export async function sendInfo(info: GameStatusInfo) {
-  if (electron && typeof electron.sendInfo === "function") {
-    electron.sendInfo(info);
-  }
+  sendInfoElectron?.(info);
   sendInfoNode(info);
 }
